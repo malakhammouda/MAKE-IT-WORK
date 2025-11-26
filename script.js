2a7f158496d967ebc9a4035ec06863cbda883516
malakhammouda <164663681+malakhammouda@users.noreply.github.com>
2025-11-26 19:01:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ setInterval(function () {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
+
+  document.getElementById("days").innerHTML = String(days).padStart(2, "0");
+  document.getElementById("hours").innerHTML = String(hours).padStart(2, "0");
+  document.getElementById("minutes").innerHTML = String(minutes).padStart(2, "0");
+  document.getElementById("seconds").innerHTML = String(seconds).padStart(2, "0");
 
   if (distance < 0) {
     document.querySelector(".countdown").innerHTML = "TIME IS UP!";
